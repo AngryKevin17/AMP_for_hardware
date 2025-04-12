02e8a91bce80069948e66b4b5cf305b52adc3e2a
ChenPenghui <cph20@mails.tsinghua.edu.cn>
2025-04-12 15:02:31 +0800

    feat: add slowwalk data and stand penalize

--- legged_gym/envs/t1/t1_amp_config.py
+++ legged_gym/envs/t1/t1_amp_config.py
@@ -134,7 +134,7 @@ class T1AMPCfg( LeggedRobotCfg ):
             collision = 0.0
             feet_stumble = 0.0 
             action_rate = -1.0
-            stand_still = 0.0
+            stand_still = -1.0
             dof_pos_limits = 0.0
         
         tracking_sigma = 0.1 # tracking reward = exp(-error^2/sigma)
